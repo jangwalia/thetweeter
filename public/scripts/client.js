713f5f7a4f7d2ab37de3d283a6df6b4cb5f586d5
jang walia <jangiwalia83@gmail.com>
2021-12-14 18:13:52 -0800

    check  cross site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function(){
- const renderTweet = function(collections){
+ 
+ 
+  const renderTweet = function(collections){
       collections.forEach(element => {
         const $tweet =  createTweetElement(element);
         return $('.tweet-container').prepend($tweet);
@@ -23,19 +25,23 @@ $(document).ready(function(){
     
   
     const createTweetElement = function(newtweet){
-      const $freshTweet = $(`<article class = "create-Tweet"> <header class = "tweet-header">
-      <img class = "userImage" src="${newtweet.user.avatars}" alt="user image"><span class = "name">${newtweet.user.name}</span><span class = "email">${newtweet.user.handle}</span>
-    </header>
-    <div class="usertweet">
-      <textarea name="usertweet" class = "show-Tweets">${newtweet.content.text}</textarea>
-    </div>
-    <footer class="tweetFooter">
-      <div >
-      <span class="timeago">${timeago.format(newtweet.created_at)}</span><span class = "reactions"><i class="fas fa-flag fa-xs"></i>  <i class="fas fa-exchange-alt fa-xs"></i>  <i class="fas fa-heart fa-xs"></i></span>
-      </div>
-    </footer>
-    </article></article>`);
-    return $freshTweet;
+
+      const $article = $(`<article>`);
+      const $header = $(`<header>`);
+      const $img = $(`<img src = "${newtweet.user.avatars}" alt = "user Image">`);
+      const $name = $(`<span class = "name">`).text(newtweet.user.name);
+      const $email = $(`<span class = "email">`).text(newtweet.user.handle);
+      const $tweetHeader = $header.append($img, $name, $email );
+      const $main = $(`<div class="tweetBody">`);
+      const $tweetBody = $(`<textarea name="usertweet" class = "show-Tweets">`).text(newtweet.content.text);
+      const $footer = $(`<footer>`);
+      const $body = $main.append($tweetBody);
+      const $timeStamp = $(` <span class="timeago">${timeago.format(newtweet.created_at)}</span>`);
+      const $reaction = $(`<span class = "reactions"><i class="fas fa-flag fa-xs"></i>  <i class="fas fa-exchange-alt fa-xs"></i>  <i class="fas fa-heart fa-xs"></i></span>`);
+      const $tweetFooter = $footer.append($timeStamp, $reaction);
+      const $newTweet =  $article.append($tweetHeader,$body,$tweetFooter);
+      return $newTweet;
+
     }
     $( "#form" ).submit(function( event ) {
       event.preventDefault();
@@ -45,16 +51,16 @@ $(document).ready(function(){
           url: '/tweets',
           data: data,
           datatype: "json",
-          success: function(msg){
-            loadTweets(msg);
-            $('#tweet-text').val('');
+          success: function(){
+            loadTweets();
+            $('#form')[0].reset();
       },
       error: function() {
          alert("please enter tweet");
       }
          
       });
-    
+      
     });
     
  })
