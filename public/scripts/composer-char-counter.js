99d8fc8ab3e8a8cade4446c246506f17e1eebb7f
jang walia <jangiwalia83@gmail.com>
2021-12-21 11:34:50 -0800

    refactor error handling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,24 @@ $(document).ready(function() {
   
 
   $('#tweet-text').on('input',function(){
+    $('.error').slideUp();
     const value = $(this).val().length;
-    let result =  140 - value;
-    
-    if(result > 0){
+    const maxLength = 140;
+    let result =  maxLength - value;
+    if(result >= 0){
       $('.counter').text(result);
+      $('.counter').addClass('morethanOne');
+      return true;
     }
     else{
       $('.error span').text("make sure the tweet is under 140 characters..!!")
       $('.error').slideDown();
-      $('.counter').addClass('lessthenOne');
+      $('.counter').removeClass('morethanOne');
+      $('.counter').addClass('lessthanOne');
       $('.counter').text(result);
+      //stop the cursor at max length which is 140 characters
+      $(this).val($(this).val().substring(0,maxLength));
+      
     }
   });
  
